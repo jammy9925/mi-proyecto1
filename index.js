a9908611b33f1d27e11eb9a41deacacbfb4f6df4
jammy <jammy.maldonado@gmail.com>
2021-01-20 20:27:58 -0600

    video 5 terminado todo

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hola mundo!!");
+const express = require ('express');
+require('dotenv').config();
+
+//crear servidor express
+const app = express();
+
+//acceso al directorio publico
+app.use(express.static('public'));
+
+//rutas
+//TODO Auth: informacion de usuario para autorizaciones
+app.use('/api/auth', require('./routes/auth'));
+
+//configuracion del puerto
+app.listen(process.env.PORT,  () => {
+console.log("backend corriendo en el puerto ${process.env.PORT}");
+})
---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+const {Router} = require ('express');
+const router = Router();
+
+router.get('/', (req,res)=>{
+
+      res.json({
+         ok: true,
+          msg:"recibido,"
+       } );
+   });
+
+   
+
+   module.exports = router;
